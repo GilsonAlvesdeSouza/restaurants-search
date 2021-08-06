1e514f19d551b9cd2a36bc68877bb63dc4bbd4c4
gilson <gilsonalvesdesouza@hotmail.com>
2021-08-05 23:41:58 -0300

    inicio do estilo do conteudo lateral

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 import theme from '../../theme';
 
-const Container = styled.div`
-  //background-color: ${() => theme.colors.primary};
+const Container = styled.aside`
+  background-color: ${() => theme.colors.background};
+  width: 360px;
+  height: 100vh;
+  overflow-y: auto;
 `;
 
 export default Container;
