1773368516493037e5c4a67fa82877ede0465666
gilson <gilsonalvesdesouza@hotmail.com>
2021-08-05 23:35:41 -0300

    criado thema, home page e reset de stilos

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import theme from '../../theme';
+
+const Container = styled.div`
+  //background-color: ${() => theme.colors.primary};
+`;
+
+export default Container;
