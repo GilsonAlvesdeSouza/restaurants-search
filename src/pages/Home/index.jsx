d4dc4817ff22edca289e463f3e80ef669f9eb36d
gilson <gilsonalvesdesouza@hotmail.com>
2021-08-06 18:22:21 -0300

    adicionado react-material-icon e refatorado o layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
-import Container from './styles';
+import React, {useState} from 'react';
+import TextField, {Input} from '@material/react-text-field';
+import {Container, Search, Logo, Wrapper, Map} from './styles';
+import logo from '../../assets/logo.svg';
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <Container>Hello Word</Container>;
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="logo do restaurante"/>
+          <TextField
+            label="Pesquisar Restaurantes"
+            outlined
+            trailingIcon={<MaterialIcon role={"button"} icon={"search "}/>}
+          >
+            <Input value={inputValue} onChange={(e) => setInputValue(e.currentTarget.value)}/>
+          </TextField>
+        </Search>
+      </Container>
+      <Map>
+
+      </Map>
+    </Wrapper>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,32 @@
 import styled from 'styled-components';
 import theme from '../../theme';
 
-const Container = styled.aside`
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Container = styled.aside`
   background-color: ${() => theme.colors.background};
   width: 360px;
   height: 100vh;
   overflow-y: auto;
 `;
 
-export default Container;
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #fff;
+  padding: 15px;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 15px;
+`;
+
+export const Map = styled.div`
+  background-color: rebeccapurple;
+  width: calc(100% - 360px);
+`;
+
